79887bf927f370b35f73fe1efca7c7091596ff52
adam-padden-13 <paddenadam@gmail.com>
2026-07-23 14:12:52 -0400

    Initial component build out

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import AudioPlayer from "./features/audio-player/AudioPlayer"
 import Footer from "./features/home/components/Footer"
 import Header from "./features/home/components/Header"
 import HomeScreen from "./features/home/HomeScreen"
 
 export default function App() {
   return (
-    <main className="mx-auto flex min-h-svh max-w-280 flex-col gap-6 px-5 pt-4 pb-4">
+    <main className="mx-auto flex min-h-svh max-w-280 flex-col gap-6 px-5 pt-4 pb-16">
       <Header />
       <HomeScreen isQRPath={location.pathname === "/qr-scan" ? true : false} />
       <Footer />
+      <AudioPlayer />
     </main>
   )
 }
---
+++ src/features/audio-player/AudioPlayer.tsx
@@ -0,0 +1,42 @@
+import { Ri24HoursFill, RiPlayCircleLine } from "@remixicon/react"
+import { useState } from "react"
+import { Drawer } from "vaul"
+
+const snapPoints = ["80px", "300px"]
+
+const AudioPlayer = () => {
+  const [snap, setSnap] = useState<number | string | null>(snapPoints[0])
+  const [isOpen, setIsOpen] = useState(true)
+
+  //   const [isPlaying, setIsPlaying] = useState(true)
+
+  return (
+    <Drawer.Root
+      snapPoints={snapPoints}
+      activeSnapPoint={snap}
+      setActiveSnapPoint={setSnap}
+      modal={false}
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      dismissible={false} // TODO: Figure out dismissable behavior
+    >
+      <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+      {!isOpen && (
+        <Drawer.Trigger className="fixed top-0 left-0 w-full items-center">
+          OPEN MUSIC PLAYER
+        </Drawer.Trigger>
+      )}
+
+      <Drawer.Portal>
+        <Drawer.Content
+          data-testid="content"
+          className="fixed inset-x-3 bottom-3 z-50 flex h-full max-h-[calc(97%-0.75rem)] flex-col rounded-xl border bg-background p-2 shadow-2xs"
+        >
+          {snap === "80px" ? <RiPlayCircleLine /> : <Ri24HoursFill />}
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
+  )
+}
+
+export default AudioPlayer
